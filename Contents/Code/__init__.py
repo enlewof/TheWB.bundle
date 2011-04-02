8b0c220914dfc472ae739c338f0aebe84d3b4bbc
sander1 <sanderspies+plexapp@gmail.com>
2011-04-02 07:30:20 +0200

    wooow this was messy :o

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,60 +1,45 @@
-import re, string, datetime
+THE_WB_ROOT       = 'http://www.thewb.com'
+THE_WB_SHOWS_LIST = 'http://www.thewb.com/shows/full-episodes'
 
-TheWB_PLUGIN_PREFIX   = "/video/TheWB"
-
-TheWB_ROOT            = "http://www.thewb.com"
-TheWB_SHOWS_LIST      = "http://www.thewb.com/shows/full-episodes"
-EP_URL             = "http://www.thewb.com/shows/full-episodes"
+ART  = 'art-default.jpg'
+ICON = 'icon-default.jpg'
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(TheWB_PLUGIN_PREFIX, MainMenu, "The WB", "icon-default.jpg", "art-default.jpg")
-  Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
-  MediaContainer.art = R('art-default.jpg')
-  MediaContainer.title1 = 'TVLand Full Episodes'
-  DirectoryItem.thumb=R("icon-default.jpg")
-  WebVideoItem.thumb=R("icon-default.jpg")
-  
-####################################################################################################
-#def MainMenu():
-#    dir = MediaContainer(mediaType='video')
-#    dir.Append(Function(DirectoryItem(allshows, "Full Episodes"), pageUrl = TheWB_SHOWS_LIST))
-#    
-#    return dir
+  Plugin.AddPrefixHandler('/video/thewb', MainMenu, 'The WB', ICON, ART)
+  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+
+  MediaContainer.art = R(ART)
+  MediaContainer.title1 = 'The WB'
+  MediaContainer.viewGroup = 'InfoList'
+
+  DirectoryItem.thumb = R(ICON)
+  WebVideoItem.thumb = R(ICON)
+
+  HTTP.CacheTime = CACHE_1HOUR
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13'
 
 ####################################################################################################
 def MainMenu():
-    dir = MediaContainer(mediaType='video')
-#    dir = MediaContainer()
-    pageUrl=TheWB_SHOWS_LIST
-    content = XML.ElementFromURL(pageUrl, True)
-    for item in content.xpath('//div[@id="show-directory"]//div/div/ul/li'):
-    	
-        
-        link = TheWB_ROOT + item.xpath("a")[0].get('href')
-        Log(link)
-        title = item.xpath("a")[0].text
-        Log(title)
-        dir.Append(Function(DirectoryItem(eplist, title=title), pageUrl=link))
-    return dir
-	
+  dir = MediaContainer()
+  content = HTML.ElementFromURL(THE_WB_SHOWS_LIST)
+
+  for item in content.xpath('//div[@id="show-directory"]//div/div/ul/li'):
+    title = item.xpath('./a')[0].text
+    link = THE_WB_ROOT + item.xpath('./a')[0].get('href')
+    dir.Append(Function(DirectoryItem(Eplist, title=title), pageUrl=link))
+
+  return dir
+
 ####################################################################################################
-def eplist(sender, pageUrl):
-    dir = MediaContainer(title2=sender.itemTitle)
-    content = XML.ElementFromURL(pageUrl, isHTML=True)
-    for item in content.xpath('//li[@id="full_ep_car1"]//div/div[@class="overlay_thumb_area"]'):
-        Log(item)
-        link =item.xpath("a")[0].get('href')
-        link=str(TheWB_ROOT) + str(link)
-        thumb = item.xpath("a/img")[0].get('src') 
-        Log(thumb)
-        title=item.xpath("a/img")[0].get('alt')
-        Log(title)
-        content2 = XML.ElementFromURL(link, isHTML="TRUE")
-        for item2 in content2.xpath('//meta[@property="og:image"]'):
-            link2 = item2.xpath('//link[@rel="video_src"]')[0].get('href')
-        Log(link2)
+def Eplist(sender, pageUrl):
+  dir = MediaContainer(title2=sender.itemTitle)
+  content = HTML.ElementFromURL(pageUrl)
 
-        dir.Append(WebVideoItem(url=link, title=title))
-    return dir
+  for item in content.xpath('//li[@id="full_ep_car1"]//div/div[@class="overlay_thumb_area"]'):
+    title = item.xpath('./a/img')[0].get('alt')
+    link = THE_WB_ROOT + item.xpath('./a')[0].get('href')
+    thumb = item.xpath('./a/img')[0].get('src')
+    dir.Append(WebVideoItem(url=link, title=title))
 
+  return dir
